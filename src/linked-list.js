507325c0cf290eb4c0d14e18c05fea83261e2360
Sergey Dmitriev <terapool@gmail.com>
2017-02-05 16:24:27 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,26 +2,25 @@ const Node = require('./node');
 
 class LinkedList {
     constructor()
-	{
-	this.lenght = 0;
-    this._head = null;
-	this._tail = null;
-
-		}
+    {
+  this.lenght = 0;
+  this._head = null;
+  this._tail = null;
+    }
 
     append(data) 
 	{ 
-		if (this.length == 0) 
-			{
-			this._head = Node(data);
-            this._tail = Node(data)
-            }
-		if (this.length > 0) 
-			{
-			this._tail.next = Node(data);
-            node.prev = this._tail;
-            this._tail = Node(data);
-            }	
+  if (this.length == 0) 
+      {
+      this._head = Node(data);
+      this._tail = Node(data)
+      }
+  if (this.length > 0) 
+	  {
+      this._tail.next = Node(data);
+      Node(data).prev = this._tail;
+      this._tail = Node(data);
+      }	
 	}
 
     head() {}
